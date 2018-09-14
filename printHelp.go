bf14c6e782d871676a01764f0dbe949a53f33836
Simon Rowe <simon@wiremoons.com>
2018-09-14 09:36:27 +0000

    Updated text so output formats correctly when '-h' is used.

--- printHelp.go
+++ printHelp.go
@@ -28,26 +28,43 @@ func printHelp() {
 
 	Are These Passwords Secure?
 	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-	While the passwords generated look far too simple and easy to be secure, the	are in fact very secure, and difficult to crack. Just because they look simple to a human - it doesn't mean they are simple to work out using a computer. They are in fact quite hard to work out for a computer. The reason for this is that they are randomly generated, not a single dictionary word, or a single common name. This makes the password harder to 'find' as it is not commonly known.
+	While the passwords generated look far too simple and easy to be secure, the are
+	in fact very secure, and difficult to crack. Just because they look simple to a
+	human - it doesn't mean they are simple to work out using a computer. They are
+	in fact quite hard to work out for a computer. The reason for this is that they
+	are randomly generated, not a single dictionary word, or a single common name.
+	This makes the password harder to 'find' as it is not commonly known.
 
-	It is a common misconception that a password has to be 'complex' to be any good. Unfortunately we have been led to believe that the more complex a password is - the better and more secure it will be - which is in fact wrong.
+	It is a common misconception that a password has to be 'complex' to be any good.
+	Unfortunately we have been led to believe that the more complex a password 
+	is - the better and more secure it will be - which is in fact wrong.
 
-	In fact a longer password, that can more easily be remembered, and therefore changed more frequently as a consequence, actually offers a far greater degree of security.
+	In fact a longer password, that can more easily be remembered, and therefore 
+	changed more frequently as a consequence, actually offers a far greater degree 
+	of security.
 
-	For more information and explanations of this, please see the web pages
-	included below under 'References'. There are plenty of expert sources on
-	the Internet also, that will explain the benefits and security of using a randomly generated three word (or more) combination password. Just remember - your password	must be at least nine characters in total - or longer if possible. You can of course	always add additional punctuation, should you wish!
+	For more information and explanations of this, please see the web pages included
+	below under 'References'. There are plenty of expert sources on the Internet
+	also, that will explain the benefits and security of using a randomly generated
+	three word (or more) combination password. Just remember - your password must
+	be at least nine characters in total - or longer if possible. You can of
+	course	always add additional punctuation, should you wish!
 
 	So How Many Possible Passwords Are There?
 	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-	There are over 1,000 three letter words in the pool that can be chosen from,and assuming you use three of these words combined, that provide 1,000^3 (1,000 to power of 3) possible combinations - of which one is your password.
+	There are over 1,000 three letter words in the pool that can be chosen from, and
+	assuming you use three of these words combined, that provide 1,000^3 (1,000 to
+	power of 3) possible combinations - of which one is your password.
 
 	So - 1,000 x 1,000 x 1,000 = 1,000,000,000 (one billion) possibilities.
 
-	If you use the mixed case option (upper and lower case) - then number increases further of course - and you can still add numbers, and/or punctuation characters if you wish too.
+	If you use the mixed case option (upper and lower case) - then number increases
+	further of course - and you can still add numbers, and/or punctuation characters
+	if you wish too.
 
-	Or just increase you password length to 12 characters, so use four of the three letter words, and you end up with 1,000,000,000,000 (one thousand billion) possibilities -and that is just lower case letters only.
-
+	Or just increase you password length to 12 characters, so use four of the three
+	letter words, and you end up with 1,000,000,000,000 (one thousand billion)
+	possibilities -and that is just lower case letters only.
 
 	References
 	¯¯¯¯¯¯¯¯¯¯
